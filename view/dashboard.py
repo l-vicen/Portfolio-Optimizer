d1ed035752a96cd2c16b4808a3998118f9ed6c86
Lucas Vicentim <lucasvicentim0301@gmail.com>
2022-01-05 10:36:07 -0300

    dep

---
+++ view/dashboard.py
@@ -0,0 +1,12 @@
+from google.oauth2 import service_account
+from gsheetsdb import connect
+import streamlit as st
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
