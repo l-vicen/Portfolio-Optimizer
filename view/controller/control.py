fc230bfc36638f84271e992c66de3563b8cccb3b
Lucas Vicentim <lucasvicentim0301@gmail.com>
2022-01-10 12:10:16 -0300

    BUG: names

--- view/controller/control.py
+++ view/controller/control.py
@@ -43,7 +43,7 @@ def return_tickers_from_names(names):
     nestedArray = df.values.tolist()
     arr = np.array(nestedArray)
 
-    return arr.flatten()
+    return arr.flatten().tolist()
 
 """
 This fuction downloads the adjusted closing price
