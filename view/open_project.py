3090c48eaef6ed1d84c49bc7ee3cb968724f6791
Lucas Vicentim <lucasvicentim0301@gmail.com>
2022-01-10 22:20:55 -0300

    BUG: feedback

--- view/open_project.py
+++ view/open_project.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
 
 def visualize_feedbacks():
     
